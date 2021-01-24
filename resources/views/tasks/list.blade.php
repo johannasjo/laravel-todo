f577af3f9111f7ad51a9216ca763e79b649db363
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-01-24 21:34:50 +0100

    layout and style

--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -13,14 +13,14 @@
     <body>
 
 
-<h1 class="d-flex justify-content-center">Tasks</h1>
+<h1 class="d-flex justify-content-center m-4">Tasks</h1>
 @if(Session::has('delete-message'))
 <div class="alert alert-danger">{{session('delete-message')}}</div>
 @elseif(Session::has('create-message'))
 <div class="alert alert-success">{{session('create-message')}}</div>
 @endif
 
-    <table class="table-bordered d-flex justify-content-center">
+    <table class="table-bordered d-flex justify-content-center m-3">
         <tr>
             <th>Title</th>
             <th>Task</th>
@@ -37,11 +37,15 @@
             <form method="post" action="{{route('tasks.delete', $task->id)}}" enctype="multipart/form-data">
             @csrf
             @method('DELETE')
+
             <td><button class="btn btn-danger m-2" type="submit">Delete</button></td>
+
             </form>
         </tr>
     @endforeach
 </table>
+<div class="d-flex justify-content-end mr-5 p-3">
     <a href="{{route('tasks.create')}}" class="btn btn-primary lm-10" type="button">Create new task</a>
+</div>
 </body>
 </html>
